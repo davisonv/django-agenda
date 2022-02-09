cfbb6ac5a225b55ff384d310d64999209c898121
davisonv <davison.vinicius360@gmail.com>
2022-02-08 23:34:26 -0300

    traduz django admin para português, configura os models no django admin

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,14 @@
+from calendar import c
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'categoria', 'telefone', 'email',)
+    list_display_links = ('nome', 'sobrenome',)
+    search_fields = ('nome', 'sobrenome',)
+    list_per_page = 10
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
+
--- contatos/models.py
+++ contatos/models.py
@@ -5,12 +5,17 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
 
 class Contato(models.Model):
     nome = models.CharField(max_length=30)
     sobrenome = models.CharField(max_length=30, blank=True)
-    telefone = models.CharField(max_length=9)
+    telefone = models.CharField(max_length=12)
     email = models.CharField(max_length=50, blank=True)
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
