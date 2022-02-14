5523fd1e3d79c10115dc48913ffc46c112228683
davisonv <davison.vinicius360@gmail.com>
2022-02-14 17:27:29 -0300

    adiciona funcionalidade de busca

--- contatos/urls.py
+++ contatos/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('busca/', views.busca, name='busca'),
     path('contato/<int:contato_id>', views.ver_contato, name='contato'),
+    
     
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator
 from .models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 def index(request):
     contatos = Contato.objects.order_by('-id').filter(mostrar=True)
@@ -17,3 +19,20 @@ def ver_contato(request, contato_id):
     if not contato.mostrar:
         raise Http404()
     return render(request, 'contatos/ver_contato.html', {'contato': contato})
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+    campos = Concat('nome', Value(' '), 'sobrenome')
+    
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+        
+    paginator = Paginator(contatos, 10)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    
+    return render(request, 'contatos/index.html', {'contatos': contatos})
