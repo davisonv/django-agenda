9938ded271e2dd96c5f3eace4a1a994db4244bb8
davisonv <davison.vinicius360@gmail.com>
2022-02-14 00:52:14 -0300

    orderna e filtra contatos, corrige bugs

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.core.paginator import Paginator
 from .models import Contato
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(mostrar=True)
     paginator = Paginator(contatos, 10)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -12,4 +13,7 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+    
+    if not contato.mostrar:
+        raise Http404()
     return render(request, 'contatos/ver_contato.html', {'contato': contato})
