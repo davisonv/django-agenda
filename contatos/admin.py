b271fb5f6cd7c689425a0d23d1a6d35acb69a87d
davisonv <davison.vinicius360@gmail.com>
2022-02-13 23:09:13 -0300

    adciona campos no model e no admin,  trata exceções

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,11 +4,12 @@ from .models import Categoria, Contato
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome', 'categoria', 'telefone', 'email',)
-    list_display_links = ('nome', 'sobrenome',)
+    list_display = ('nome', 'sobrenome', 'categoria', 'telefone', 'email', 'mostrar',)
+    list_display_links = ('nome', 'sobrenome')
     search_fields = ('nome', 'sobrenome',)
     list_per_page = 10
-
+    list_editable = ('telefone', 'mostrar',)
+    
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
 
